af0de4d867a93e2735f7cc7c9b2bc5ee169c4a5b
Md <shoyebmd424@gmail.com>
2023-07-12 15:55:31 +0530

    fixed

--- client/src/Axios/index.jsx
+++ client/src/Axios/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL:
-    "http://localhost:8080/" || "https://rablo-product-app.onrender.com:8080/",
+  baseURL: "https://rablo-product-app.onrender.com:8080/",
 });
 export default Axios;
+//  "http://localhost:8080/" ||
